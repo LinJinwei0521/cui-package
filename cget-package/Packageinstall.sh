4ae4dd984c227ec84302c07b47fc2b735c4ba6de
LinJinwei0521 <163630445+LinJinwei0521@users.noreply.github.com>
2024-11-16 21:40:57 +0800

    Update Packageinstall.sh

--- cget-package/Packageinstall.sh
+++ cget-package/Packageinstall.sh
@@ -1,5 +1,6 @@
 apt install libcurl
 wget https://raw.githubusercontent.com/LinJinwei0521/cuou-package/refs/heads/main/cget-package/Cget.c
-gcc -o cget cget.c -lcurl
-rm cget.c
+wget https://raw.githubusercontent.com/LinJinwei0521/cuou-package/refs/heads/main/cget-package/cuoupackage.cpc
+gcc -o cget Cget.c -lcurl
+rm Cget.c
 rm Packageinstall.sh
