f43281466a2330167b0dbf22ceb4f89bcbffc88b
LinJinwei0521 <163630445+LinJinwei0521@users.noreply.github.com>
2024-11-18 19:04:20 +0800

    Delete cget-package directory

--- cget-package/Packageinstall.sh
+++
@@ -1,6 +0,0 @@
-apt install libcurl
-wget https://raw.githubusercontent.com/LinJinwei0521/cuou-package/refs/heads/main/cget-package/Cget.c
-wget https://raw.githubusercontent.com/LinJinwei0521/cuou-package/refs/heads/main/cget-package/cuoupackage.cpc
-gcc -o cget Cget.c -lcurl
-rm Cget.c
-rm Packageinstall.sh
